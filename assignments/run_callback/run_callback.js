d77df5065534061e0f79dfc020c6f9453b2ac736
SriramSololearner <55018057+SriramSololearner@users.noreply.github.com>
2022-03-09 23:00:36 +0530

    Assignment-6

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,5 +1,9 @@
 function RunCallback(a, b, cb) {
     // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
+    let sum = a+b;
+    return cb(sum);
+
 }
+// RunCallback(3,4,cb);
 
 module.exports = RunCallback;
